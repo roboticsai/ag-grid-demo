bf3ad5fff30e4348a6083c9367dacbc3562281f5
roboticsai <dinesh99981@gmail.com>
2023-02-10 16:27:43 +0545

    thf

---
+++ components/gridPopupMenu/Tabs.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+
+export default function Tabs(){
+    const [activeTab, setActiveTab] = useState(
+        {
+            panel: 'hidden',
+            filter: 'visible'  
+        }
+    );
+
+    
+      const  btnStyle = 'm-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
+    
+      let panel_visibility,filter_visibility
+    
+      function tabClicked(tab){
+        if(tab=='panel') {
+          setActiveTab({
+            panel: 'visible',
+            filter: 'hidden'
+          })
+        }
+        else if(tab=='filter') {
+          setActiveTab({
+            panel: 'hidden',
+            filter: 'visible'
+          })
+        }
+      }
+    return (
+        <>
+        <div className="bg-white w-full border-b border-gray-200 dark:border-gray-700">
+            <ul className="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
+                <li className="mr-2">
+                    <a onClick={tabClicked('panel')} href="#" className="inline-flex p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                    </a>
+                </li>
+                <li className="mr-2">
+                    <a onClick={tabClicked('filter')} href="#" className="inline-flex p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500 group" aria-current="page">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z" />
+                    </svg>
+                    </a>
+                </li>
+                <li className="mr-2">
+                    <a onClick={tabClicked('select')} href="#" className="inline-flex p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M9 4.5v15m6-15v15m-10.875 0h15.75c.621 0 1.125-.504 1.125-1.125V5.625c0-.621-.504-1.125-1.125-1.125H4.125C3.504 4.5 3 5.004 3 5.625v12.75c0 .621.504 1.125 1.125 1.125z" />
+                    </svg>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div className={`${panel_visibility} p-4 rounded-lg bg-gray-50 dark:bg-gray-800`} aria-labelledby="profile-tab">
+              <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong class="font-medium text-gray-800 dark:text-white">Profile tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
+        </div>
+        <div className={`${filter_visibility} p-4 rounded-lg bg-gray-50 dark:bg-gray-800`} aria-labelledby="dashboard-tab">
+            <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong class="font-medium text-gray-800 dark:text-white">Dashboard tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
+        </div>
+        </>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,57 @@
+import React, { useCallback, useMemo, useRef, useState } from 'react';
+import { render } from 'react-dom';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+
+// import styles from '@/styles/Home.module.css'
+import GridPopupMenu from '../components/gridPopupMenu/gridPopupMenu'
+
+export default function Home() {
+  const [rowData, setRowData] = useState();
+  const [columnDefs, setColumnDefs] = useState([
+    { 
+      field: 'athlete',
+      // headerComponent: GridPopupMenu, 
+      headerComponent: GridPopupMenu,
+    },
+    { field: 'age' },
+    { field: 'country' },
+    { field: 'year' },
+    { field: 'date' },
+    { field: 'sport' },
+    { field: 'gold' },
+    { field: 'silver' },
+    { field: 'bronze' },
+    { field: 'total' },
+  ]);
+  
+  const defaultColDef = useMemo(() => {
+    return {
+      editable: true,
+      sortable: true,
+      filter: true,
+      resizable: true,
+    };
+  }, []);
+
+  const onGridReady = useCallback((params) => {
+    fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+      .then((resp) => resp.json())
+      .then((data) => setRowData(data));
+  }, []);
+
+  return (
+    <>  
+      <div style={{height: 600, width: '100%'}} className="ag-theme-alpine static">
+        <AgGridReact
+          rowData={rowData}
+          columnDefs={columnDefs}
+          defaultColDef={defaultColDef}
+          onGridReady={onGridReady}
+        ></AgGridReact>
+      </div>
+    </>
+  )
+}
